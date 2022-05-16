ec3758fea2ac130f779a82c9dac8af749f73e6c9
Justyna <j.matejkowska@gmail.com>
2022-05-16 12:29:04 +0200

    dodanie wyjątku oraz zaokrąglenie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-el = list(map(int,input("Wprowadź najpierw n później k oddzielając je spacją: ").strip().split()))
+
+el = list(map(int,input().strip().split()))
 
 if len(el)>=3:
   print('Podałeś za dużo liczb')
@@ -9,6 +10,8 @@ elif len(el)==2:
   def symbol_newton(el):   #wzór
     return math.factorial(el[0])/(math.factorial(el[1])*math.factorial(el[0]-el[1]))
   if el[0]>el[1] and el[1]>0: #tylko liczby dodatnie całkowite 
-    print(symbol_newton(el))
+    print(math.ceil(symbol_newton(el)))
+  elif el[0]==el[1] or el[1]==0:
+    print('1')
   else: 
     print('Błąd-n musi być większe od k') 
